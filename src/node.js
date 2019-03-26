e0c9c907dfc0c6ed520790748dde975a7819c3b9
slava-ff <slavik-28@mail.ru>
2019-03-26 22:52:41 +0300

    feat: work a few

--- src/node.js
+++ src/node.js
@@ -1,21 +1,47 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-
+		if (this.left != null && this.right != null) {
+			return;
+		} else
+		if (this.left != null) {
+			this.right = node;
+		} else {this.left = node;}
+		node.parent = this; // ?????????????????????????????
 	}
 
 	removeChild(node) {
-
+		if (this.left === node) {
+			this.left = null;
+		} else 
+		if (this.right === node) {
+			this.right = null;
+		} else {
+			throw new Error();
+		}
+		node.parent = null;
+		//console.log('removeChild: ', this);
 	}
 
 	remove() {
-
+		if (this.parent) {
+			this.parent.removeChild(this);
+		}
+		//console.log('this: ', this);
 	}
 
 	swapWithParent() {
+		if (!this.parent) {
+			return;
+		}
+		
 		
 	}
 }
